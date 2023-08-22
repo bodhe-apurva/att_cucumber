ae28c70b6d051eb6294bc0179f93240fcf038eec
Gourav <gourav@g.com>
2023-08-22 08:41:38 +0530

    committing for the first time

---
+++ src/test/java/runners/LoginRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features = {""}
+		
+		
+		
+		
+		)
+
+public class LoginRunner {
+
+}
---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,41 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+	
+	@Given("User is at login page")
+	public void preConditions() {
+		System.out.println("Pre conditions got executed");
+	}
+	
+	@When("User enters the username")
+	public void enterUsername()
+	{
+		System.out.println("Entering username");
+	}
+	
+	@When("User enters the password")
+	public void enterPassword()
+	{
+		System.out.println("Entering password");
+	}
+	
+	
+	@When("User clicks on login button")
+	public void clickOnLogin()
+	{
+		System.out.println("Clicking on login button");
+	}
+	
+	@Then("User redirects to home page")
+	public void validateLogin()
+	{
+		System.out.println("Validation of login");
+	}
+	
+	
+
+}
